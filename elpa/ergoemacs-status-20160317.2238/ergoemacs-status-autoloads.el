ccccfa2911e1b4fabc15cf3fe7445d07b67233ea
ando <ito-yu@ntt-el.com>
2018-07-24 15:36:37 +0900

    darkroom add

--- elpa/ergoemacs-status-20160317.2238/ergoemacs-status-autoloads.el
+++
@@ -1,23 +0,0 @@
-;;; ergoemacs-status-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-(add-to-list 'load-path (directory-file-name
-                         (or (file-name-directory #$) (car load-path))))
-
-
-;;;### (autoloads nil "ergoemacs-status" "ergoemacs-status.el" (0
-;;;;;;  0 0 0))
-;;; Generated autoloads from ergoemacs-status.el
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "ergoemacs-status" '("ergoemacs-")))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; ergoemacs-status-autoloads.el ends here
--- elpa/ergoemacs-status-20160317.2238/ergoemacs-status-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "ergoemacs-status" "20160317.2238" "Adaptive Status Bar / Mode Line" '((powerline "2.3") (mode-icons "0.1.0")))
