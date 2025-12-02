05bcae5061541117500e007d4517d89b5ae23192
Akari-Kaminoto <39481351+Akari-Kaminoto@users.noreply.github.com>
2025-12-02 17:15:46 +0900

    Fix add-to-load-path function to ensure correct path

--- init.el
+++ init.el
@@ -38,7 +38,8 @@
   (let (path)
     (dolist (path paths paths)
       (let ((default-directory
-              (expand-file-name (concat user-emacs-directory path))))        (add-to-list 'load-path default-directory)
+              (expand-file-name (concat user-emacs-directory path))))
+ 		   (add-to-list 'load-path default-directory)
         (if (fboundp 'normal-top-level-add-subdirs-to-load-path)
             (normal-top-level-add-subdirs-to-load-path))))))
 
@@ -76,7 +77,6 @@
       (goto-char (point-max))
       (eval-print-last-sexp)))
   (load bootstrap-file nil 'nomessage))
-
 ;;emacs Ver27以上は以下がいる
 ;(setq package-enable-at-startup nil)
 ;
